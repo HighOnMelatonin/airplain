d1ce4e9697938f0b3dbc8392730121d498560c75
cw-han <133126313+LeddyNull@users.noreply.github.com>
2025-07-30 15:23:20 +0800

    Added vocareum project template

---
+++ wh stuff.py
@@ -0,0 +1,14 @@
+# To setup venv:
+    # In terminal,
+    # Run: Set-ExecutionPolicy Unrestricted -Scope Process
+    # Run: .venv\Scripts\activate     
+
+# To push
+    # Commit on my branch, then push on my branch (sync)
+    #Go to github, and send pull request
+    #Then tell Rui Anh-sama to approve
+
+# To pull
+    # git checkout master (switches to master branch)
+    # git pull
+    # git checkout weihan (switch back to my branch)
