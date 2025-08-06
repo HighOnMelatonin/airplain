1d988272540182d446ab589bab7316f9e20bd798
Scarsoftime <188433924+Otniel-times@users.noreply.github.com>
2025-08-06 19:27:33 +0800

    camelCase on linreg file

--- linearRegression.py
+++ linearRegression.py
@@ -3,16 +3,16 @@ import numpy as np
 import pandas as pd
 import cleanNether as cn
 
-def get_features_targets(df: pd.DataFrame, 
-                         feature_names: list[str], 
-                         target_names: list[str]) -> tuple[pd.DataFrame, pd.DataFrame]:
+def getFeaturesTargets(df: pd.DataFrame, 
+                         featureNames: list[str], 
+                         targetNames: list[str]) -> tuple[pd.DataFrame, pd.DataFrame]:
     '''
     Gets the features and targets arrays from a DataFrame.
 
     Args:
         df (pd.DataFrame): The dataframe to get the features and target from.
-        feature_names (list[str]): The names of the columns to use as features.
-        target_names (list[str]): The names of the columns to use as targets.
+        featureNames (list[str]): The names of the columns to use as features.
+        targetNames (list[str]): The names of the columns to use as targets.
 
     Functionality:
         - Locates the features and targets columns from df and returns them as dataframes.
@@ -20,22 +20,22 @@ def get_features_targets(df: pd.DataFrame,
     Returns:
         tuple[pd.DataFrame, pd.DataFrame]: The features dataframe and the targets dataframe.
     '''
-    features: pd.DataFrame = df.copy().loc[:,feature_names]
-    df_target: pd.DataFrame = df.copy().loc[:, target_names]
+    features: pd.DataFrame = df.copy().loc[:,featureNames]
+    dfTarget: pd.DataFrame = df.copy().loc[:, targetNames]
     
-    return features, df_target
+    return features, dfTarget
 
-def split_data(df_feature: pd.DataFrame, df_target: pd.DataFrame, 
-               random_state: Optional[int]=None, 
-               test_size: float=0.5) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+def splitData(dfFeature: pd.DataFrame, dfTarget: pd.DataFrame, 
+               randomState: Optional[int]=None, 
+               testSize: float=0.5) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
     '''
     Splits the feature and target dataframes for training and testing.
     
     Args:
-        df_feature (pd.DataFrame): Dataframe of the features.
-        df_target (pd.DataFrame): Dataframe of the targets.
-        random_state = None (Optional[int]): The seed to use for the random splitting of the data.
-        test_size = 0.5 (float): The percentage of the initial data to use as test data.
+        dfFeature (pd.DataFrame): Dataframe of the features.
+        dfTarget (pd.DataFrame): Dataframe of the targets.
+        randomState = None (Optional[int]): The seed to use for the random splitting of the data.
+        testSize = 0.5 (float): The percentage of the initial data to use as test data.
     
     Returns:
         tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]: The training features, test features, training targets, and test targets.
@@ -46,23 +46,23 @@ def split_data(df_feature: pd.DataFrame, df_target: pd.DataFrame,
         - Creates the training data sets by removing the indices from the data
     '''
     
-    np.random.seed(random_state)
-    size: int = df_feature.shape[0]
-    test_indices: np.ndarray = np.random.choice(size, int(size*test_size), False)
+    np.random.seed(randomState)
+    size: int = dfFeature.shape[0]
+    test_indices: np.ndarray = np.random.choice(size, int(size*testSize), False)
     
-    df_feature_test: pd.DataFrame = df_feature.loc[test_indices,:]
-    df_target_test: pd.DataFrame = df_target.loc[test_indices,:]
-    df_feature_train: pd.DataFrame = df_feature.drop(index=test_indices) 
-    df_target_train: pd.DataFrame = df_target.drop(index=test_indices) 
+    dfFeatureTest: pd.DataFrame = dfFeature.loc[test_indices,:]
+    dfTargetTest: pd.DataFrame = dfTarget.loc[test_indices,:]
+    dfFeatureTrain: pd.DataFrame = dfFeature.drop(index=test_indices) 
+    dfFeatureTest: pd.DataFrame = dfTarget.drop(index=test_indices) 
 
-    return df_feature_train, df_feature_test, df_target_train, df_target_test
+    return dfFeatureTrain, dfFeatureTest, dfFeatureTest, dfTargetTest
 
-def prepare_feature(np_feature: np.ndarray) -> np.ndarray:
+def prepareFeature(npFeature: np.ndarray) -> np.ndarray:
     '''
     Prepare an array for linear regression.
     
     Args:
-        np_feature (np.ndarray): The array to prepare.
+        npFeature (np.ndarray): The array to prepare.
 
     Functionality:
         - Converts the array to a np.ndarray if it is a pd.DataFrame
@@ -71,55 +71,55 @@ def prepare_feature(np_feature: np.ndarray) -> np.ndarray:
     Returns:
         np.ndarray: The prepared array.
     '''
-    if isinstance(np_feature, pd.DataFrame):
+    if isinstance(npFeature, pd.DataFrame):
         array: np.ndarray = array.to_numpy()
-    np_feature: np.ndarray = np_feature
-    one_array: np.ndarray = np.ones((np_feature.shape[0],1))
-    result: np.ndarray = np.concatenate((one_array, np_feature), axis=1)
+    npFeature: np.ndarray = npFeature
+    oneArray: np.ndarray = np.ones((npFeature.shape[0],1))
+    result: np.ndarray = np.concatenate((oneArray, npFeature), axis=1)
     return result
 
-def normalize_z(array: np.ndarray, columns_means: Optional[np.ndarray]=None,
-                columns_stds: Optional[np.ndarray]=None) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+def normalizeZ(array: np.ndarray, columnsMeans: Optional[np.ndarray]=None,
+                columnsStds: Optional[np.ndarray]=None) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
     '''
     Perform z-normalization on an array.
 
     Args:
         array (np.ndarray): The array to normalize.
-        columns_means = None(Optional[np.ndarray]): The means to use for normalization.
-        columns_stds = None (Optional[np.ndarray]): The stds to use for normalization.
+        columnsMeans = None(Optional[np.ndarray]): The means to use for normalization.
+        columnsStds = None (Optional[np.ndarray]): The stds to use for normalization.
         
     Returns:
         tuple[np.ndarray, np.ndarray, np.ndarray]: The normalized array, the means used for normalizing, and the stds used for normalizing.
 
     Functionality:
         - Converts the array to a np.ndarray if it is a pd.DataFrame
-        - Sets columns_means and columns_stds to those of the array's columns if either is not entered.
+        - Sets columnsMeans and columnsStds to those of the array's columns if either is not entered.
         - Normalizes a copy of the array with z-normlization
 
     Raises:
         AssertionError: When the size of the means or stds passed in do not match the size of the array.
     '''
-    assert columns_means is None or columns_means.shape == (1, array.shape[1])
-    assert columns_stds is None or columns_stds.shape == (1, array.shape[1])
+    assert columnsMeans is None or columnsMeans.shape == (1, array.shape[1])
+    assert columnsStds is None or columnsStds.shape == (1, array.shape[1])
 
     if isinstance(array, pd.DataFrame):
         array: np.ndarray = array.to_numpy()
 
     out: np.ndarray = array.copy()
-    if columns_means is None:
-        columns_means: np.ndarray = array.mean(axis=0).reshape(1, -1)
-    if columns_stds is None:
-        columns_stds: np.ndarray = array.std(axis=0).reshape(1, -1)
+    if columnsMeans is None:
+        columnsMeans: np.ndarray = array.mean(axis=0).reshape(1, -1)
+    if columnsStds is None:
+        columnsStds: np.ndarray = array.std(axis=0).reshape(1, -1)
 
-    out: np.ndarray = (out - columns_means) / columns_stds
+    out: np.ndarray = (out - columnsMeans) / columnsStds
     
     assert out.shape == array.shape
-    assert columns_means.shape == (1, array.shape[1])
-    assert columns_stds.shape == (1, array.shape[1])
+    assert columnsMeans.shape == (1, array.shape[1])
+    assert columnsStds.shape == (1, array.shape[1])
 
-    return out, columns_means, columns_stds
+    return out, columnsMeans, columnsStds
 
-def calc_linreg(X: np.ndarray, beta: np.ndarray) -> np.ndarray:
+def calcLinreg(X: np.ndarray, beta: np.ndarray) -> np.ndarray:
     '''
     Calculates the resulting values from the linear regression.
     
@@ -141,7 +141,7 @@ def calc_linreg(X: np.ndarray, beta: np.ndarray) -> np.ndarray:
     assert result.shape == (X.shape[0], 1)
     return result
 
-def compute_cost_linreg(X: np.ndarray, y: np.ndarray, beta: np.ndarray) -> np.ndarray:
+def computeCostLinreg(X: np.ndarray, y: np.ndarray, beta: np.ndarray) -> np.ndarray:
     '''
     Computes the cost function for linear regression.
     
@@ -159,14 +159,14 @@ def compute_cost_linreg(X: np.ndarray, y: np.ndarray, beta: np.ndarray) -> np.nd
     Raises:
         AssertionError: When the resulting cost is off the wrong shape.
     '''
-    y_hat: np.ndarray = calc_linreg(X, beta)
-    y_diff: np.ndarray = (y_hat - y)
-    diff_sums: np.ndarray = np.matmul( y_diff.T, y_diff )
-    J: np.ndarray = diff_sums / ( 2 * X.shape[0] )
+    yHat: np.ndarray = calcLinreg(X, beta)
+    yDiff: np.ndarray = (yHat - y)
+    diffSums: np.ndarray = np.matmul( yDiff.T, yDiff )
+    J: np.ndarray = diffSums / ( 2 * X.shape[0] )
     assert J.shape == (1, 1)
     return np.squeeze(J)
 
-def gradient_descent_linreg(X: np.ndarray, y: np.ndarray, beta: np.ndarray,   
+def gradientDescentLinreg(X: np.ndarray, y: np.ndarray, beta: np.ndarray,   
                             alpha: float, num_iters: int) -> tuple[np.ndarray, np.ndarray]:
     '''
     Compute the coefficients for linear regression via the gradient descent algorithim.
@@ -192,19 +192,19 @@ def gradient_descent_linreg(X: np.ndarray, y: np.ndarray, beta: np.ndarray,
     J_storage: np.ndarray = np.zeros( (num_iters, 1) )
         
     for i in range(num_iters):
-        y_hat: np.ndarray = calc_linreg(X, beta)
-        y_diff: np.ndarray = y_hat - y
-        diff_sum: np.ndarray = np.matmul( X.T, y_diff )
-        beta: np.ndarray = beta - (alpha / m * diff_sum)
-        J: np.ndarray = compute_cost_linreg( X, y, beta )
+        yHat: np.ndarray = calcLinreg(X, beta)
+        yDiff: np.ndarray = yHat - y
+        diffSum: np.ndarray = np.matmul( X.T, yDiff )
+        beta: np.ndarray = beta - (alpha / m * diffSum)
+        J: np.ndarray = computeCostLinreg( X, y, beta )
         J_storage[i] = J
         
     assert beta.shape == (X.shape[1], 1)
     assert J_storage.shape == (num_iters, 1)
     return beta, J_storage
 
-def build_model_linreg(df_feature_train: pd.DataFrame,
-                       df_target_train: pd.DataFrame,
+def buildModelLinreg(dfFeatureTrain: pd.DataFrame,
+                       dfFeatureTest: pd.DataFrame,
                        beta: Optional[np.ndarray] = None,
                        alpha: float = 0.01,
                        iterations: int = 1500) -> tuple[dict[str, Any], np.ndarray]:
@@ -212,8 +212,8 @@ def build_model_linreg(df_feature_train: pd.DataFrame,
     Builds a linear regression model for the given features and targets.
     
     Args:
-        df_feature_train (pd.DataFrame): Dataframe of the training features.
-        df_target_train (pd.DataFrame): Dataframe of the training targets.
+        dfFeatureTrain (pd.DataFrame): Dataframe of the training features.
+        dfFeatureTest (pd.DataFrame): Dataframe of the training targets.
         beta = None (Optional[np.ndarray]): The initial predicted coefficients for the linear regression
         alpha = 0.01 (float): The learning rate for the gradient descent algorithim.
         iterations = 1500 (int): The number of iterations to run the gradient descent algorithim.
@@ -231,44 +231,44 @@ def build_model_linreg(df_feature_train: pd.DataFrame,
     '''
 
     if beta is None:
-        beta = np.zeros((df_feature_train.shape[1] + 1, 1)) 
-    assert beta.shape == (df_feature_train.shape[1] + 1, 1)
+        beta = np.zeros((dfFeatureTrain.shape[1] + 1, 1)) 
+    assert beta.shape == (dfFeatureTrain.shape[1] + 1, 1)
 
     model: dict[str, Any] = {}
     
     
-    array_feature_train: np.ndarray = df_feature_train.to_numpy()
-    array_feature_train_z: np.ndarray; means: np.ndarray; stds: np.ndarray
-    array_feature_train_z, means, stds = normalize_z(array_feature_train)
+    arrayFeatureTrain: np.ndarray = dfFeatureTrain.to_numpy()
+    arrayFeatureTrainZ: np.ndarray; means: np.ndarray; stds: np.ndarray
+    arrayFeatureTrainZ, means, stds = normalizeZ(arrayFeatureTrain)
 
-    X = prepare_feature(array_feature_train_z)
-    y: np.ndarray = df_target_train.to_numpy()
+    X = prepareFeature(arrayFeatureTrainZ)
+    y: np.ndarray = dfFeatureTest.to_numpy()
 
-    beta, J_storage = gradient_descent_linreg(X, y, beta, alpha, iterations)
+    beta, J_storage = gradientDescentLinreg(X, y, beta, alpha, iterations)
 
     model['beta'], model['means'], model['stds'] = beta, means, stds
     
-    assert model["beta"].shape == (df_feature_train.shape[1] + 1, 1)
-    assert model["means"].shape == (1, df_feature_train.shape[1])
-    assert model["stds"].shape == (1, df_feature_train.shape[1])
+    assert model["beta"].shape == (dfFeatureTrain.shape[1] + 1, 1)
+    assert model["means"].shape == (1, dfFeatureTrain.shape[1])
+    assert model["stds"].shape == (1, dfFeatureTrain.shape[1])
     assert J_storage.shape == (iterations, 1)
     return model, J_storage
 
-def predict_linreg(array_feature: np.ndarray, beta: np.ndarray, 
+def predictLinreg(arrayFeature: np.ndarray, beta: np.ndarray, 
                    means: Optional[np.ndarray]=None, 
                    stds: Optional[np.ndarray]=None) -> np.ndarray:
     '''
     Gets the predicted straight line equation from the given features and coefficients.
     
     Args:
-        array_feature (np.ndarray): The array of features.
+        arrayFeature (np.ndarray): The array of features.
         beta (np.ndarray): The coefficients found from linear regression. 
         means = None (Optional[np.ndarray]): The means to use for normalization.
         stds = None (Optional[np.ndarray]): The stds to use for normalization.
 
     Functionality:
         - Converts the array to a np.ndarray if it is a pd.DataFrame.
-        - Sets columns_means and columns_stds to those of the array's columns if either is not entered.
+        - Sets columnsMeans and columnsStds to those of the array's columns if either is not entered.
         - Normalizes the array with z-normalization then prepares it for computation.
         - Computes the predicted target values.
     
@@ -278,18 +278,18 @@ def predict_linreg(array_feature: np.ndarray, beta: np.ndarray,
     Raises:
         AssertionError: When the size of the means or stds passed in do not match the size of the array.
     '''
-    assert means is None or means.shape == (1, array_feature.shape[1])
-    assert stds is None or stds.shape == (1, array_feature.shape[1])
-    if isinstance(array_feature, pd.DataFrame):
-        array_feature: np.ndarray = array_feature.to_numpy()
-    X, _, _ = normalize_z(array_feature, means, stds)
-    X = prepare_feature(X)
-    result: np.ndarray = calc_linreg(X, beta)
+    assert means is None or means.shape == (1, arrayFeature.shape[1])
+    assert stds is None or stds.shape == (1, arrayFeature.shape[1])
+    if isinstance(arrayFeature, pd.DataFrame):
+        arrayFeature: np.ndarray = arrayFeature.to_numpy()
+    X, _, _ = normalizeZ(arrayFeature, means, stds)
+    X = prepareFeature(X)
+    result: np.ndarray = calcLinreg(X, beta)
     
-    assert result.shape == (array_feature.shape[0], 1)
+    assert result.shape == (arrayFeature.shape[0], 1)
     return result
 
-def r2_score(y: np.ndarray, ypred: np.ndarray) -> float:
+def r2Score(y: np.ndarray, ypred: np.ndarray) -> float:
     '''
     Calculates the r2 score of the linear regression.
     
@@ -301,15 +301,15 @@ def r2_score(y: np.ndarray, ypred: np.ndarray) -> float:
         float: The calculated r2 score.
 
     Functionality:
-        - Computes ( 1 - (ss_res / ss_tot) ). 
+        - Computes ( 1 - (ssRes / ss_tot) ). 
     '''
-    y_diff: np.ndarray = y - ypred
-    ss_res: np.ndarray = np.sum( y_diff.T @ y_diff )
-    y_mean_diff: np.ndarray = y - y.mean()
-    ss_tot: np.ndarray = np.sum( y_mean_diff.T @ y_mean_diff )
-    return float(1 - (ss_res / ss_tot))
+    yDiff: np.ndarray = y - ypred
+    ssRes: np.ndarray = np.sum( yDiff.T @ yDiff )
+    yMeanDiff: np.ndarray = y - y.mean()
+    ss_tot: np.ndarray = np.sum( yMeanDiff.T @ yMeanDiff )
+    return float(1 - (ssRes / ss_tot))
 
-def mean_squared_error(target: np.ndarray, pred: np.ndarray) -> float:
+def meanSquaredError(target: np.ndarray, pred: np.ndarray) -> float:
     '''
     Calculates the mean squared error of the linear regression.
     
@@ -321,11 +321,11 @@ def mean_squared_error(target: np.ndarray, pred: np.ndarray) -> float:
         float: The calculated mean squared error.
 
     Functionality:
-        - Computes 1 / n * ( y_i - y_hat_i) ** 2
+        - Computes 1 / n * ( y_i - yHat_i) ** 2
     '''
 
-    y_diff: np.ndarray = target - pred
-    total: np.ndarray = np.sum(y_diff.T @ y_diff)
+    yDiff: np.ndarray = target - pred
+    total: np.ndarray = np.sum(yDiff.T @ yDiff)
     n: int = target.shape[0]
     return float(total / n)
 
@@ -430,10 +430,10 @@ if __name__ == '__main__':
     ### Linear regression, don't run until dataset is fixed
     # features = ['0 to 10 km', '>10 to 20 km', '>20 to 50km', 'Population Density']
     # target = ['PM2.5']
-    # data_features, data_target = get_features_targets(data, features, target) 
-    # data_features_train, data_features_test, data_target_train, data_target_test = split_data(data_features, data_target, random_state=100, test_size=0.3)
-    # model, J_storage = build_model_linreg(data_features_train, data_target_train)
-    # pred: np.ndarray = predict_linreg(data_features_test.to_numpy(), model['beta'], model['means'], model['stds'])
+    # dataFeatures, dataTarget = getFeaturesTargets(data, features, target) 
+    # dataFeaturesTrain, dataFeaturesTest, dataTarget_train, dataTarget_test = splitData(dataFeatures, dataTarget, randomState=100, testSize=0.3)
+    # model, J_storage = buildModelLinreg(dataFeaturesTrain, dataTarget_train)
+    # pred: np.ndarray = predictLinreg(dataFeaturesTest.to_numpy(), model['beta'], model['means'], model['stds'])
 
     # import matplotlib.pyplot as plt
     # import matplotlib.axes as axes
@@ -445,7 +445,7 @@ if __name__ == '__main__':
 
 
     # for feature in features:
-    #     plt.scatter(data_features_test[feature], data_target_test)
-    #     plt.scatter(data_features_test[feature], pred)
+    #     plt.scatter(dataFeaturesTest[feature], dataTarget_test)
+    #     plt.scatter(dataFeaturesTest[feature], pred)
 
 
